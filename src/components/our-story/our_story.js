63eeeea62110e6afd3040d5971c65209571eff47
giovanni <giovanni@aimusic.co.uk>
2020-02-11 10:03:17 +0000

    refactor(Css style and add some text): Css style and add some text

--- src/components/our-story/our_story.js
+++ src/components/our-story/our_story.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React from 'react'
 
 export default function OurStory() {
-    return (
-        <section className="hero">
-            {/* TODO: Need to change the pic*/}
-            <div className="ourStory">
-                <p className="storyParagraph">
-                    Our story began in the fancy neighbourhood of South
-                    Kensington over a cup of coffee. However, we were the ones
-                    making and serving it.
-                </p>
-                <p className="storyParagraph">
-                    6 amazing years later and we’ve travelled the world
-                    exploring it, both learning and mastering a new language,
-                    settling into a home just the two of us, and finding our
-                    dream job – we cannot wait for this new chapter to begin!
-                </p>
-                {/*  TODO: Remember to change the below text*/}
-                <p className="storyParagraph">
-                    6 amazing years later and we’ve travelled the world
-                    exploring it, both learning and mastering a new language,
-                    settling into a home just the two of us, and finding our
-                    dream job – we cannot wait for this new chapter to begin!
-                </p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="hero">
+      <div className="ourStory">
+        <p className="storyParagraph">
+          A slightly different story to most, but this is our unique one. It all begun back in 2013, in fair London we set our scene, in the fancy
+          neighbourhood of South Kensington. Some said it was destiny, some said it was love but we like to call it “magic”.
+        </p>
+        <p className="storyParagraph">
+          It was only another noisy day in the Italian restaurant, Carluccios, but that day Vincenzo and Vera’s life was about to change.
+        </p>
+        <p className="storyParagraph">
+          The story of two star-crossed lovers was about to start. The crazy Vincenzo, was making his living as a barista, but London had far more
+          challenges for him.
+        </p>
+        <p className="storyParagraph">
+          He did not know that the providence was preserving him something special and one day, Vera burnished as a Brightstar, "she doth teach the
+          torches to burn bright!".
+        </p>
+        <p className="storyParagraph">
+          Vincenzo couldn’t stand such beauty and instantly he realised that his heart did not know love until now. But once again the challenge was
+          in every morning and the language gave him a warning.
+        </p>
+        <p className="storyParagraph">
+          The first attempts from the Italian chap were, actually, very crap, but soon Vera decided to start. Beauty too rich for use, for Earth too
+          dear. Every day he tried.
+          <p className="storyParagraph">
+            Written notes and broken verse. The two lovers engaged in a mix of Italo-Portu-Spanish language and a whole lot of hand gestures. The
+            destiny finally worked in their favour.
+          </p>
+        </p>
+        <p className="storyParagraph">
+          Fast forward 6 years later, off they go to embark in yet another adventure – And live the rest of their lives together!
+        </p>
+      </div>
+    </section>
+  )
 }
--- src/components/wedding-itinerary/wedding_itinerary.js
+++ src/components/wedding-itinerary/wedding_itinerary.js
@@ -2,27 +2,33 @@ import React from 'react'
 
 export default function WeddingItinerary() {
   return (
-    <div className="column">
+    <div>
       <div className="hero">
         <div className="hero-body">
           <h1 className="hWeddingItinerary"> Ceremony, Cocktail, Reception and Party </h1>
           <p className="paragraphWeddingItinerary"> Quinta Casa Portuguesa – Colares, Sintra Estrada Nacional N247, 2705-655 </p>
           <p className="paragraphWeddingItinerary"> Please join us at 16:00 to begin the celebration! </p>
           <p className="paragraphWeddingItinerary">
-            Dress code: Formal attire The ceremony will be held outdoors, so we advise ladies not to wear any stiletto high heels. Temperatures may
-            also drop in the evening, so please bring a little something to keep you warm!
+            <b>Dress code:</b> Formal attire The ceremony will be held outdoors, so we advise ladies not to wear any stiletto high heels. Temperatures
+            may also drop in the evening, so please bring a little something to keep you warm!
           </p>
         </div>
       </div>
-      <div className="mapWeddingItinerary">
-        <iframe
-          title="location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49756.983224713986!2d-9.498577322855613!3d38.79095656081532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ece7534ec342f%3A0xc141684ae435b078!2sQuinta%20Casa%20Portuguesa!5e0!3m2!1sen!2suk!4v1580672092761!5m2!1sen!2suk"
-          width="600"
-          height="450"
-          frameBorder="0"
-          allowFullScreen=""
-        ></iframe>
+      <div className="columns location">
+        <div className="column is-half">
+          <figure>
+            <img src="/images/location.jpg" alt="bride" />
+          </figure>
+        </div>
+        <div class="column is-half">
+          <div className="videoWrapper">
+            <iframe
+              title="location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49756.983224713986!2d-9.498577322855613!3d38.79095656081532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ece7534ec342f%3A0xc141684ae435b078!2sQuinta%20Casa%20Portuguesa!5e0!3m2!1sen!2suk!4v1580672092761!5m2!1sen!2suk"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   )
