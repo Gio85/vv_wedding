b5697553cfbb4b828e09935fbde1434c5f9df2ee
giovanni <giovanni@aimusic.co.uk>
2020-02-25 23:45:36 +0000

    Updates

--- precache-manifest.061ffdc3f35c25b038400c678ba6283c.js
+++ precache-manifest.061ffdc3f35c25b038400c678ba6283c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "56dc6770c9cfdb24d0181e45a0cb2367",
+    "revision": "c6509731ed965c8d9353dde5e2598c91",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.1c2325a6.chunk.css"
   },
   {
-    "revision": "1771e6956c59cda7be6c",
-    "url": "/static/css/main.944ff233.chunk.css"
+    "revision": "abc2012753ad72ec9de2",
+    "url": "/static/css/main.977fba28.chunk.css"
   },
   {
     "revision": "2085bfdd684f3e702f78",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d2e58c05.chunk.js.LICENSE"
   },
   {
-    "revision": "1771e6956c59cda7be6c",
+    "revision": "abc2012753ad72ec9de2",
     "url": "/static/js/main.5b17304a.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cef37dc6cf267cbe3dab31303052cdae.js"
+  "/precache-manifest.061ffdc3f35c25b038400c678ba6283c.js"
 );
 
 self.addEventListener('message', (event) => {
